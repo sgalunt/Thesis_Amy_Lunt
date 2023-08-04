e7bb39095570ff51a980007dd61b83e112676bdf
sgalunt <57002035+sgalunt@users.noreply.github.com>
2023-08-04 14:16:45 +0100

    LoadEightWRackYumiStation.java

--- LoadEightWRackYumiStation.java
+++ LoadEightWRackYumiStation.java
@@ -1,40 +1,4 @@
-package crystalBot.tasks.yumiStation;
 
-import robotChemist.interfaces.GenericTwoFingerGripper.GripperPos;
-import robotChemist.interfaces.LBRCommander;
-import robotChemist.tasks.RackHandling;
-import robotChemist.utility.Rack;
-
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import com.kuka.roboticsAPI.applicationModel.IApplicationData;
-import com.kuka.roboticsAPI.geometricModel.ObjectFrame;
-import com.kuka.roboticsAPI.geometricModel.Workpiece;
-import com.kuka.task.ITaskLogger;
-
-public class LoadEightWRackYumiStation
-{
-	private LBRCommander commander;
-	private RackHandling rackHandling;
-	private ITaskLogger logger;
-	private IApplicationData applicationData;
-	private Rack jobRack;
-	private boolean needCalibration;
-
-	
-	public LoadEightWRackYumiStation(LBRCommander commander, Workpiece rack, RackHandling rackHandling, boolean needCalibration, IApplicationData applicationData,ITaskLogger logger)
-	{
-		this.commander = commander;
-		this.rackHandling = rackHandling;
-		this.applicationData = applicationData;
-		this.logger = logger;
-		ObjectFrame rackFrame = applicationData.getFrame("/RobotRacks/DeckRack1");
-		this.jobRack = new Rack(rack,rackFrame);
-		this.needCalibration = needCalibration;
-	}
-	
-	public void run() throws Exception{
 		
 	// Calibration at YumiStation
 		
